3fc7a43a03ba4e412cf11d76266c6f8385c2101d
estevennac <93159877+estevennac@users.noreply.github.com>
2021-10-30 16:12:00 -0500

    Add files via upload

---
+++ Primer parcial/Deberes/Esteven Olmedo/Ejercicio Sabado 30-10-2021/plantillaDiv.h
@@ -0,0 +1,23 @@
+/***********************************************************************
+ * Universidad de las Fuerzas Armadas Espe
+ * Module:  Aplicativo
+ * Author:  Esteven Nacimba
+ * Fecha de creacion: 30/10/2021 15:38 pm
+ * Fecha de modificación:  
+ * Materia: Estructura de Datos Nrc: 7167
+ ***********************************************************************/
+
+
+#include <iostream>
+#include <stdlib.h>
+
+template <typename P>
+class division{
+	public:
+		division(P, P);
+		P getDiv();
+	
+	private:
+		P num1,num2;
+	
+};
