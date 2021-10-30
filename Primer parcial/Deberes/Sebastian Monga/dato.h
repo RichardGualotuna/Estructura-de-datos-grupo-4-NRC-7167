773935fc90684df3a2ea174b5c8e476763c920b0
sebas-09 <93159185+sebas-09@users.noreply.github.com>
2021-10-29 21:26:37 -0500

    Add files via upload
    Ejercicio devuelve el máximo de 3 valores

---
+++ Primer parcial/Deberes/Sebastian Monga/dato.h
@@ -0,0 +1,19 @@
+/**
+ *              UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+ * @file MAXIMO DE 3 ARGUMENTOS 
+ * @author:  Sebastian Monga
+ * @date: 2021-10-29
+ * @date_modifiques: 2021-10-29
+ * NRC: 7166
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <iostream>
+template <typename T>
+class Dato{
+	public:
+		
+		T maximo(T d1,T d2,T d3);
+	private:
+		
+};
