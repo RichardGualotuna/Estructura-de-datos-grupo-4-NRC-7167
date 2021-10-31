80cfbf7299081ba9d4da0702431e7ac3f358ed0e
RichardGualotuna <rfgualotuna@espe.edu.ec>
2021-10-31 10:06:13 -0500

    Deber sabado

--- Primer parcial/Deberes/Richard Gualotuña/Ejercicio sabado 30-10-2021 plantillas(suma)/main.cpp
+++ Primer parcial/Deberes/Richard Gualotuña/Ejercicio sabado 30-10-2021 plantillas(suma)/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
-
+/* UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
+nombre del programa: sumas
+Autor: Gualotuña Richard
+Fecha de creaccion: 30-10-2021
+Fecha de  modificacion: 31-10-2021
+NRC: 7167
+Estructura de datos*/
 template <class T>
 class suma{
 	public:
