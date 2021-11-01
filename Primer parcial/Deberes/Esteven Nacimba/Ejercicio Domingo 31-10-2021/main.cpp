fb0e776329e71c6ea3c41b7b35f6066a620a6f93
estevennac <93159877+estevennac@users.noreply.github.com>
2021-10-31 21:36:01 -0500

    Add files via upload

---
+++ Primer parcial/Deberes/Esteven Nacimba/Ejercicio Domingo 31-10-2021/main.cpp
@@ -0,0 +1,32 @@
+/***********************************************************************
+ * Universidad de las Fuerzas Armadas Espe
+ * Module:  Aplicativo
+ * Author:  Esteven Nacimba 21:18pm
+ * Fecha de creacion: 31/10/2021 21 pm
+ * Fecha de modificación:  
+ * Materia: Estructura de Datos Nrc: 7167
+ ***********************************************************************/
+
+#include <iostream>
+#include <stdlib.h>
+#include "division.cpp"
+
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+
+	int num1,num2;
+	float num3,num4;
+	
+	cout<<"Ingresar primer numero: ";
+	cin>>num1;
+	cout<<"Ingresar segundo numero: ";
+	cin>>num2;
+	
+	cout<<mayor(num1,num2)<<" es el numero mayor ";
+
+	
+	return 0;
+}
---
+++ Primer parcial/Deberes/Esteven Nacimba/Ejercicio Domingo 31-10-2021/mayor.h
@@ -0,0 +1,20 @@
+/***********************************************************************
+ * Universidad de las Fuerzas Armadas Espe
+ * Module:  Aplicativo
+ * Author:  Esteven Nacimba 21:18pm
+ * Fecha de creacion: 31/10/2021 21 pm
+ * Fecha de modificación:  
+ * Materia: Estructura de Datos Nrc: 7167
+ ***********************************************************************/
+
+#include <iostream>
+#include <stdlib.h>
+
+template <typename T>
+T mayor (T n1, T n2)
+{
+	return (n1 > n2)?n1:n2;
+	
+}
+
+
